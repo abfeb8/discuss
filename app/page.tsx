ee1fd8c74cd5975582a7d1a6f1dd7ef405ac1bb9
Abhishek <abfeb8@gmail.com>
2025-02-23 11:40:48 +0530

    added next-ui (HeroUI)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+'use client';
+
+import { Button } from "@nextui-org/react";
+
 export default function Home() {
     return (
-        <div>Home Page</div>
+        <div>
+            <Button>NextUI Button!!</Button>
+        </div>
     )
 }
