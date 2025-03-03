dfaada130e53caca509e86c23db11953a425be4a
Abhishek <abfeb8@gmail.com>
2025-03-03 22:56:58 +0530

    updated paths util

--- app/utils/paths.ts
+++ app/utils/paths.ts
@@ -1,16 +1,16 @@
 const paths = {
-    home(): string {
-        return '/';
-    },
-    topicShow(topicSlug: string): string {
-        return `/topics/${topicSlug}/`;
-    },
-    postCreate(topicSlug: string): string {
-        return `/topics/${topicSlug}/posts/new`;
-    },
-    postShow(topicSlug: string, postId: string): string {
-        return `/topics/${topicSlug}/posts/${postId}`;
-    }
+	home(): string {
+		return '/pages/';
+	},
+	topicShow(topicSlug: string): string {
+		return `/pages/topics/${topicSlug}/`;
+	},
+	postCreate(topicSlug: string): string {
+		return `/pages/topics/${topicSlug}/posts/new`;
+	},
+	postShow(topicSlug: string, postId: string): string {
+		return `/pages/topics/${topicSlug}/posts/${postId}`;
+	},
 };
 
 export default paths;
