fd407a0c43f505de63f30a11ecf4842f95cfb3b7
Abhishek <abfeb8@gmail.com>
2025-03-03 22:57:40 +0530

    form implementaion for new topic creation

--- app/actions/create-topic.ts
+++ app/actions/create-topic.ts
@@ -1,12 +1,79 @@
 'use server';
 
-export async function createTopic(formData: FormData) {
-    // TODO: revalidate the homepage
-    
-    const topicName = formData.get("name");
-    const topicDesc = formData.get("description");
-    
-    console.log(`create new topic action called with ${topicName + " " + topicDesc}`);
+import { z } from 'zod';
+import { db } from '../db';
+import { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import paths from '../utils/paths';
+import { revalidatePath } from 'next/cache';
+import { auth } from '../auth/auth';
 
+const createTopicSchema = z.object({
+	name: z
+		.string()
+		.min(3)
+		.regex(
+			/^[a-zA-Z0-9_]+$/,
+			'Only alphanumeric characters and underscores are allowed'
+		),
+	description: z.string().min(10),
+});
 
+interface CreateTopicFormState {
+	errors: {
+		name?: string[];
+		description?: string[];
+		_form?: string[];
+	};
+}
+
+export async function createTopic(
+	formState: CreateTopicFormState,
+	formData: FormData
+): Promise<CreateTopicFormState> {
+	const topicName = formData.get('name');
+	const topicDesc = formData.get('description');
+
+	console.log(
+		`create new topic action called with Topic: ${topicName} Description: ${topicDesc}`
+	);
+
+	const result = createTopicSchema.safeParse({
+		name: topicName,
+		description: topicDesc,
+	});
+
+	if (!result.success) {
+		console.log(
+			'createTopic action failed with errors:',
+			result.error.flatten().fieldErrors
+		);
+		return { errors: result.error.flatten().fieldErrors };
+	}
+
+	const session = await auth();
+	if (!session || !session.user) {
+		return { errors: { _form: ['You must be logged in!!'] } };
+	}
+
+	let topic: Topic;
+	try {
+		topic = await db.topic.create({
+			data: {
+				slug: result.data.name,
+				description: result.data.description,
+			},
+		});
+	} catch (error) {
+		console.error('createTopic action failed with error:', error);
+		if (error instanceof Error) {
+			return { errors: { _form: [error.message] } };
+		}
+		return { errors: { _form: ['something went wrong'] } };
+	}
+
+	console.log('createTopic action succeeded');
+
+	revalidatePath(paths.home());
+	redirect(paths.topicShow(topic.slug));
 }
--- app/components/topics/topic-create-form.tsx
+++ app/components/topics/topic-create-form.tsx
@@ -1,24 +1,67 @@
 'use client';
 
-import { createTopic } from "@/app/actions";
-import { Popover, PopoverTrigger, PopoverContent, Button, Input, Textarea } from "@heroui/react";
+import { createTopic } from '@/app/actions';
+import {
+	Popover,
+	PopoverTrigger,
+	PopoverContent,
+	Button,
+	Input,
+	Textarea,
+	Form,
+} from '@heroui/react';
+import { startTransition, useActionState } from 'react';
 
 export default function TopicCreateForm() {
-    return (
-        <Popover placement="left">
-            <PopoverTrigger>
-                <Button color="primary">Create Topic</Button>
-            </PopoverTrigger>
-            <PopoverContent>
-                <form action={createTopic}>
-                    <div className="flex flex-col gap-4 p-4 w-80">
-                        <h3 className="text-lg">Create a Topic</h3>
-                        <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
-                        <Textarea name="description" label="Description" labelPlacement="outside" placeholder="Topic Description" />
-                        <Button type="submit">Submit</Button>
-                    </div>
-                </form>
-            </PopoverContent>
-        </Popover>
-    );
+	const [formState, action] = useActionState(createTopic, { errors: {} });
+
+	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		console.log('handleSubmit called');
+
+		event.preventDefault();
+		const formData = new FormData(event.target as HTMLFormElement);
+		startTransition(() => {
+			action(formData);
+		});
+	}
+
+	return (
+		<Popover placement="bottom-end">
+			<PopoverTrigger>
+				<Button color="primary">Create Topic</Button>
+			</PopoverTrigger>
+			<PopoverContent>
+				<Form onSubmit={handleSubmit}>
+					<div className="flex flex-col gap-4 p-4 w-80">
+						<h3 className="text-lg">Create a Topic</h3>
+						<Input
+							autoFocus
+							name="name"
+							label="Name"
+							labelPlacement="outside"
+							placeholder="Name"
+							isInvalid={!!formState.errors.name}
+							errorMessage={formState.errors.name?.join(', \n')}
+						/>
+						<Textarea
+							name="description"
+							label="Description"
+							labelPlacement="outside"
+							placeholder="Topic Description"
+							isInvalid={!!formState.errors.description}
+							errorMessage={formState.errors.description?.join(
+								', '
+							)}
+						/>
+						{formState.errors._form ? (
+							<div className="p-2 rounded border border-red-400 bg-red-100">
+								{formState.errors._form.join(',')}
+							</div>
+						) : null}
+						<Button type="submit">Submit</Button>
+					</div>
+				</Form>
+			</PopoverContent>
+		</Popover>
+	);
 }
